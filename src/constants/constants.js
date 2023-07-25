2bd6f8fc942aa50d4381899ebba1a6577d8dff4a
Anubhav <aadhikari1248@gmail.com>
2023-07-25 19:17:11 +0530

    added 2 projects

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,12 +1,22 @@
 export const projects = [
   {
+    title: 'AI Image Generator',
+    description: 
+    "A fully responsive text-to-image AI web app built with React that generates 1024x1024 images based on the prompt provided. The user can pick their own prompt or choose from a predefined array of 50 prompts using the 'Surprise Me' button. Once the image has been generated, it is pushed to the home feed where the user can view all his generated images and download them.",
+    image: '/images/ai.png',
+    tags: ['React', 'Tailwind', 'MongoDB', 'Express'],
+    source: 'https://github.com/anubhav1206/AI-image-generator',
+    visit: 'https://anubhav-ai-image-generator.netlify.app/',
+    id: 0,
+  },
+  {
     title: 'Space travel Website',
     description: "An interactive and immersive space travel website built using HTML CSS and Js built with the collaboration of Kevin Powell and scrimba. The site has 4 pages the main page with an explore button that leads to the destination page the crew page and the technology page",
     image: '/images/1.png',
     tags: ['HTML', 'CSS', 'Js'],
     source: 'https://github.com/anubhav1206/the-space-travel-website',
     visit: 'https://anubhav-space-travel-website.netlify.app/',
-    id: 0,
+    id: 1,
   },
   {
     title: 'Meme Generator',
@@ -15,7 +25,7 @@ export const projects = [
     tags: ['React', 'Tailwind CSS', 'NextJs'],
     source: 'https://github.com/anubhav1206/meme-generator',
     visit: 'https://anubhav-meme-generator.vercel.app/',
-    id: 1,
+    id: 2,
   },
   {
     title: 'Password generator',
@@ -24,18 +34,29 @@ export const projects = [
     tags: ['HTML', 'CSS', 'Javascript'],
     source: 'https://github.com/anubhav1206/password-generator',
     visit: 'https://anubhav-password-generator.netlify.app/',
-    id: 2,
+    id: 5,
   },
   {
-    title: 'Twitter Clone',
+    title: 'Movie Saver',
     description: 
-    "A Twitter clone built using HTML, CSS, and javascript (no frameworks). It is based on real Twitter where the user can tweet, interact with others’ tweets (those tweets that are stored in the data array), and also interact with his own tweets. The user can like, reply and retweet a previously made tweet and upon doing so, the color of the icon changes that shows that the user has interacted with the tweet.",
-    image: '/images/4.png',
-    tags: ['HTML', 'CSS', 'Javascript'],
-    source: 'https://github.com/anubhav1206/twitter-clone',
-    visit: 'https://anubhav-twitter-clone.netlify.app/',
+    "Movie-Saver is a user-friendly web application designed to simplify the process of searching for and saving movies to your personal watchlist. With a clean and intuitive UI and a dark mode toggle, it allows you to effortlessly discover new movies and keep track of the ones you plan to watch.",
+    image: '/images/movie.png',
+    tags: ['HTML', 'CSS', 'React'],
+    source: 'https://github.com/anubhav1206/Movie-Saver',
+    visit: 'https://anubhav-movie-watchlist.netlify.app/index.html',
     id: 3,
   },
+  {
+    title: 'Role Playing Game',
+    description: 
+    "Movie-Saver is a user-friendly web application designed to simplify the process of searching for and saving movies to your personal watchlist. With a clean and intuitive UI and a dark mode toggle, it allows you to effortlessly discover new movies and keep track of the ones you plan to watch.",
+    image: '/images/game.png',
+    tags: ['HTML', 'CSS', 'Javascript'],
+    source: 'https://github.com/anubhav1206/wizard-orc-game',
+    visit: 'https://anubhav-role-playing-game.netlify.app/',
+    id: 4,
+  },
+
 ];
 
 export const TimeLineData = [
